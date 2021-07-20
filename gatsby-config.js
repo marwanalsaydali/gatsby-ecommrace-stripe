45063b3ec0343de032ef5e8b88bbeebd0152a8bc
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-07-20 10:23:39 -0400

    Stripe key to .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     resolve: `gatsby-source-stripe`,
     options: {
       objects: ['Balance', 'BalanceTransaction', 'Product', 'ApplicationFee', 'Sku', 'Subscription'],
-      secretKey: 'stripe_secret_key_here',
+      secretKey: process.env.STRIPE,
       downloadFiles: true,
     }
   },
